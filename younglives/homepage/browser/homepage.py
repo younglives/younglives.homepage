bc69014ebf3c2f6b04e71ea58d304ffbb86b3732
Michael Davis <m.r.davis@me.com>
2012-01-08 14:30:21 +0000

    Add a toLocalizedDate method to the view, this seems to be the new preferred way of doing this

--- younglives/homepage/browser/homepage.py
+++ younglives/homepage/browser/homepage.py
@@ -68,3 +68,10 @@ class HomePageView(BrowserView):
                 image = eval("self.context.getBox%sImage()" % box),
                 links = [{'title':x.Title,'url':x.absolute_url} for x in links],))
         return results
+
+    def toLocalizedTime(self, time, long_format=None, time_only = None):
+        """Convert time to localized time
+        """
+        util = getToolByName(self.context, 'translation_service')
+        return util.ulocalized_time(time, long_format, time_only, self.context,
+                                    domain='plonelocales')
