486253042d42747605b84647bd314b65c1135a42
Michael Davis <m.r.davis@me.com>
2014-01-29 20:54:20 +0000

    Disconnect the event for now and remove the pdb prompt

--- younglives/homepage/content/events.py
+++ younglives/homepage/content/events.py
@@ -2,7 +2,6 @@ from zope.interface import noLongerProvides
 from Products.CMFCore.utils import getToolByName
 
 def removeInterfaces(ob, event):
-    import pdb;pdb.set_trace()
     catalog = getToolByName(self, 'portal_catalog')
     brains = catalog(object_provides='younglives.content.interfaces.content.IBannerAware')
     
